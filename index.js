9e0775d3b92cbe651ba651423cdae5a26ed1e0d0
eerojala <eero.ojala@helsinki.fi>
2018-02-06 00:05:17 +0200

    3.6 done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const apiPersons = '/api/persons'
+
+app.use(bodyParser.json())
 
 let persons = [
     {
@@ -24,6 +27,10 @@ let persons = [
         id: 4
     }
 ]
+
+const generateId = () => {
+    return Math.floor((Math.random() * 10000) + 1)
+}
 
 app.get(`${apiPersons}`, (req, res) => {
     res.json(persons)
@@ -51,10 +58,33 @@ app.get(`${apiPersons}/:id`, (req, res) => {
 })
 
 app.delete(`${apiPersons}/:id`, (req, res) => {
+    console.log('DELETE')
     const id = Number(req.params.id)
     persons = persons.filter(person => person.id !== id)
 
     res.status(204).end()
+})
+
+app.post(`${apiPersons}`, (req, res) => {
+    console.log('posted')
+    const body = req.body
+    
+    if (!body.name || !body.number ) {
+        return res.status(400).json({error: 'Missing vital parameters'})
+    } else if (persons.find(person => person.name === body.name)) {
+        return res.status(400).json({error: 'Name must be unique'})
+    } 
+    
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+
+    res.json(person)
 })
 
 const PORT = 3001
