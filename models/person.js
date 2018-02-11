02ca4e1247ca3243bf40f6b7437914882dd4cffb
eerojala <eero.ojala@helsinki.fi>
2018-02-11 17:32:44 +0200

    Week 3 done

--- models/person.js
+++ models/person.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
-const dbUrl = 'mongodb://eerojala:kebabjohnson@ds225308.mlab.com:25308/phonebook_database'
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const dbUrl = process.env.MONGODB_URI
 
 mongoose.connect(dbUrl)
 
